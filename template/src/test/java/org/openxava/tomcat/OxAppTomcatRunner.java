8c90cbe4c932250d6fdddd153ae4b93e3cbcc11b
Jean-Eric <jec@jesc.ch>
2014-02-01 22:33:36 +0100

    Config of TomcatRunner

--- template/src/test/java/org/openxava/tomcat/OxAppTomcatRunner.java
+++ template/src/test/java/org/openxava/tomcat/OxAppTomcatRunner.java
@@ -4,19 +4,19 @@ import tomcat.TomcatRunner;
 
 import java.io.IOException;
 
-public class OpenXavaTomcatRunner extends TomcatRunner {
+public class OxAppTomcatRunner extends TomcatRunner {
 
 	public static void main(String[] args) throws Exception {
 
 		final String webappDir = "webapp";
 
-		final String contextPath = "/ox-app";
+		final String contextPath = "/oxapp";
 
-		final OpenXavaTomcatRunner runner = new OpenXavaTomcatRunner(".", contextPath, 8083, webappDir, null, null);
+		final OxAppTomcatRunner runner = new OxAppTomcatRunner(".", contextPath, 8083, webappDir, null, null);
 		runner.start();
 	}
 
-    public OpenXavaTomcatRunner(String currentDir, String contextPath, Integer port, String webappDir, String contextXmlPath, String userDatabasePath) throws IOException {
+    public OxAppTomcatRunner(String currentDir, String contextPath, Integer port, String webappDir, String contextXmlPath, String userDatabasePath) throws IOException {
         super(currentDir, contextPath, port, webappDir, contextXmlPath, userDatabasePath);
     }
 
