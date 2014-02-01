b592e7fddf990ece05065c5318b24c266ef8c4e6
Jean-Eric <jec@jesc.ch>
2014-02-01 16:46:24 +0100

    Creation of an app template

--- dist/install-artifacts.sh
+++ dist/install-artifacts.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
+groupId=org.openxava.deps
 version=4.9
-
-(cd openxava-${version}/workspace/MySchool && ant)
-
+pomfile=gen-pom.xml
 lib=openxava-4.9/workspace/OpenXava/lib
+
+
+#(cd openxava-${version}/workspace/MySchool && ant)
+
+
 
 for f in $(ls $lib) ; do
 
   name=$(echo $f | sed -e 's/\.jar//')
   echo "Installing $name ..."
-  
-  mvn install:install-file -Dfile=$lib/$f \
-			    -DgroupId=org.openxava.deps \
-			    -DartifactId=$name \
-                         -Dversion=$version \
-                         -Dpackaging=jar \
-                         -DgeneratePom=true
-                         
+
+  echo "<dependency>" >> $pomfile
+  echo "<groupId>${groupId}</groupId>" >> $pomfile
+  echo "<artifactId>${name</artifactId>" >> $pomfile
+  echo "<version>${version}</version>" >> $pomfile
+  echo "</dependency>" >> $pomfile
+
+  if [ 0 = 1 ] ; then
+      mvn install:install-file -Dfile=$lib/$f \
+                    -DgroupId=$groupId \
+                    -DartifactId=$name \
+                             -Dversion=$version \
+                             -Dpackaging=jar \
+                             -DgeneratePom=true
+  fi
+
   if [ $? != 0 ]; then
     exit 1
   fi
@@ -25,4 +37,3 @@ for f in $(ls $lib) ; do
   
 
 done
-
