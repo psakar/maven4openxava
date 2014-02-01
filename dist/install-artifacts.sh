d936a065468cfe8555c61acfcbdfacf14a5a5f04
Jean-Eric <jec@jesc.ch>
2014-02-01 16:29:20 +0100

    Added official distrib of openxava 4.9

--- dist/install-artifacts.sh
+++ dist/install-artifacts.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-(cd openxava-4.9 && ant)
+version=4.9
+
+#(cd openxava-${version}/workspace/MySchool && ant)
+
+lib=openxava-4.9/workspace/OpenXava/lib
+
+for f in $(ls $lib) ; do
+
+  name=$(echo $f | sed -e 's/\.jar//')
+  echo "Installing $name ..."
+  
+  mvn install:install-file -Dfile=$lib/$f \
+			    -DgroupId=org.openxava.deps \
+			    -DartifactId=$name \
+                         -Dversion=$version \
+                         -Dpackaging=jar \
+                         -DgeneratePom=true
+                         
+  if [ $? != 0 ]; then
+    exit 1
+  fi
+  echo "$name done ..."  
+  
+
+done
 
