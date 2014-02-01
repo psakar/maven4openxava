6541309667e97d0b1028cd458491dd2d6abdc244
Jean-Eric <jec@jesc.ch>
2014-02-01 21:14:29 +0100

    Added openxava.zip in its entirety

--- dist/scripts/create-maven-project.sh
+++ dist/scripts/create-maven-project.sh
@@ -2,4 +2,8 @@
 base=openxava-4.9/workspace/OpenXava
 main_dest=artifacts/openxava
 
-cp -r $base/xava $main_dest/src/main/resources
+rm -Rf $main_dest/src/main/resources/*
+#cp -r $base/xava $main_dest/src/main/resources
+cp -r $base/properties $main_dest/src/main/resources
+
+
