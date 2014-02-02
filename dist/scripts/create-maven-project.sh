dea65436fce4e2402a7f9c66e39c8872e0c4ae47
Jean-Eric <jec@jesc.ch>
2014-02-02 15:19:22 +0100

    Split of official code from addons

--- dist/scripts/create-maven-project.sh
+++ dist/scripts/create-maven-project.sh
@@ -1,11 +1,15 @@
 
 src=openxava-4.9/workspace/OpenXava
-dest_openxava=artifacts/openxava
-dest_testing=artifacts/testing
+dest_openxava=artifacts/openxava-official
+dest_testing=artifacts/testing-official
 
+
+# openxava : mkdir
+rm -Rf $dest_openxava/src
+mkdir -p $dest_openxava/src/main/java
+mkdir -p $dest_openxava/src/main/resources
 
 # openxava : resources
-rm -Rf $dest_openxava/src/main/resources/*
 cp -r $src/xava/* $dest_openxava/src/main/resources
 cp -r $src/properties/* $dest_openxava/src/main/resources
 cp -r $src/bin/* $dest_openxava/src/main/resources
@@ -13,12 +17,11 @@ cp -r $src/web $dest_openxava/src/main/resources/xava
 rm $dest_openxava/src/main/resources/xava/*.jsp
 
 # openxava : src/main
-rm -Rf $dest_openxava/src/main/java/*
 cp -r $src/src/* $dest_openxava/src/main/java
 rm -Rf $dest_openxava/src/main/java/org/openxava/tests
 
 # testing : src/main
-rm -Rf $dest_testing/src/main/java/org
+rm -Rf $dest_testing/src
 mkdir -p $dest_testing/src/main/java/org/openxava
 cp -r $src/src/org/openxava/tests $dest_testing/src/main/java/org/openxava
 
