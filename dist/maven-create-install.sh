896335bb2a72472e8e18d1ebfb742632dd5f993a
Jean-Eric Cuendet <jean-eric.cuendet@vd.ch>
2014-02-04 16:01:31 +0100

    Version 4.9.1 of OpenXava

--- dist/maven-create-install.sh
+++ dist/maven-create-install.sh
@@ -1,9 +1,11 @@
 
+version=4.9.1
+
 # Build OpenXava
 (cd openxava/workspace/MySchool && ant)
 
 # Install OpenXava deps
-scripts/install-openxava-deps.sh
+scripts/install-openxava-deps.sh $version
 
 # Generate and install OpenXava artifact
 scripts/create-maven-project.sh
